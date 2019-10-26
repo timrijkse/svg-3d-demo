d084706e31fb3db3fe3a000b30374332ab81c269
Tim Rijkse <trijkse@gmail.com>
2019-10-26 13:28:25 +0200

    Rotate svg on mousemove

--- src/components/SvgAnimation.js
+++ src/components/SvgAnimation.js
@@ -1,9 +1,48 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const SvgContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    perspective: 500px;
+    background: green;
+`
+
+const SvgAnimationComponent = styled.svg.attrs(props => ({
+    style: {
+        width: props.width,
+        height: props.height,
+        transform: getSkew(props.width, props.height, props.mouseX, props.mouseY)
+    }
+}))`
+    transition: transform 0.5s ease-out;
+    will-change: transition;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    background-color: red;
+`
+
+const getSkew = (width, height, mouseX, mouseY) => {
+    // const rotateX = (mouseY / height / 2).toFixed(2)
+    // const rotateY = (mouseX / width / 2).toFixed(2)
+    
+    const rotateY = map(mouseX, 0, width, -10, 10)
+    const rotateX = map(mouseY, 0, height, -10, 10)
+
+    console.log(rotateX, rotateY)
+
+    return `translate(-50%, -50%) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
+}
+
+const map = (x, in_min, in_max, out_min, out_max) => {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
 class SvgAnimation extends React.Component {
     state = {
-        windowHeight: undefined,
-        windowWidth: undefined,
+        windowHeight: 0,
+        windowWidth: 0,
         mouseX: 0,
         mouseY: 0
     }
@@ -14,12 +53,19 @@ class SvgAnimation extends React.Component {
     });
 
     componentDidMount() {
-        this.handleResize();
         window.addEventListener('resize', this.handleResize)
+        this.handleResize()
     }
 
     componentWillUnmount() {
         window.removeEventListener('resize', this.handleResize)
+    }
+
+    handleResize = (e) => {
+        this.setState(state => ({
+            windowWidth: window.innerWidth,
+            windowHeight: window.innerHeight,
+        }))
     }
 
     handleMouseMove = (e) => {
@@ -32,9 +78,19 @@ class SvgAnimation extends React.Component {
     }
 
     render () {
-        return <div onMouseMove={this.handleMouseMove}>
-            {this.state.mouseX}
-        </div>
+        const viewBox = `0 0 ${this.state.windowWidth} ${this.state.windowHeight}`
+
+        return <SvgContainer>
+            <SvgAnimationComponent 
+                onMouseMove={this.handleMouseMove} 
+                width={this.state.windowWidth}
+                height={this.state.windowHeight}
+                mouseX={this.state.mouseX}
+                mouseY={this.state.mouseY}
+                viewBox={viewBox}>
+                {this.state.mouseX}
+            </SvgAnimationComponent>
+        </SvgContainer>
     }
 }
 
